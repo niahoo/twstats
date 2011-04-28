eb378383d71d16f066a80d021c08e01acb31f348
Ludovic Demblans <ludovic@demblans.com>
2011-04-28 15:40:25 +0200

    latin1 again

--- twstats.sql
+++ twstats.sql
@@ -1,19 +1,18 @@
-
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
-
 DROP TABLE IF EXISTS `counters`;
+
 CREATE TABLE `counters` (
   `id` int(10) NOT NULL auto_increment,
-  `strkey` varchar(50) character set utf8 collate utf8_unicode_ci NOT NULL,
-  `display_name` varchar(100) character set utf8 collate utf8_unicode_ci NOT NULL,
+  `strkey` varchar(50) NOT NULL,
+  `display_name` varchar(100) NOT NULL,
   `section_id` int(10) NOT NULL,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `counter_by_section` (`strkey`,`section_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
-
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `counters_days`;
+
 CREATE TABLE `counters_days` (
   `counter_id` int(10) NOT NULL,
   `day_date` date NOT NULL,
@@ -21,15 +20,15 @@ CREATE TABLE `counters_days` (
   `visits` int(10) NOT NULL,
   `day_visits` int(10) NOT NULL,
   PRIMARY KEY  (`counter_id`,`day_date`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `sections`;
+
 CREATE TABLE `sections` (
   `id` int(10) NOT NULL auto_increment,
-  `name` varchar(50) character set latin1 collate latin1_general_ci NOT NULL,
+  `name` varchar(50) NOT NULL,
   `display_name` varchar(255) NOT NULL,
   `parent_id` int(10) NOT NULL,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `unicite_nomEtParent` (`name`,`parent_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;

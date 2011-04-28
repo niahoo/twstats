fefec11ed953abba83fa9706eb3301f3ab2fb5d7
Ludovic Demblans <ludovic@demblans.com>
2011-04-28 15:09:47 +0200

    added drop table to the dump

--- twstats.sql
+++ twstats.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS `counters`;
 CREATE TABLE `counters` (
   `id` int(10) NOT NULL auto_increment,
   `strkey` varchar(50) character set utf8 collate utf8_unicode_ci NOT NULL,
@@ -5,9 +6,10 @@ CREATE TABLE `counters` (
   `section_id` int(10) NOT NULL,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `counter_by_section` (`strkey`,`section_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 
+DROP TABLE IF EXISTS `counters_days`;
 CREATE TABLE `counters_days` (
   `counter_id` int(10) NOT NULL,
   `day_date` date NOT NULL,
@@ -18,11 +20,11 @@ CREATE TABLE `counters_days` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
-
+DROP TABLE IF EXISTS `sections`;
 CREATE TABLE `sections` (
   `id` int(10) NOT NULL auto_increment,
   `name` varchar(50) character set latin1 collate latin1_general_ci NOT NULL,
   `parent_id` int(10) NOT NULL,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `unicite_nomEtParent` (`name`,`parent_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
